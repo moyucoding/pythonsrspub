f895cc052390b1bc693459b144218e5705f6241c
moyucoding <tharealzhong@gmail.com>
2024-12-05 10:51:48 +0800

    Update rtmppublisher.py

--- rtmppublisher.py
+++ rtmppublisher.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def publish_stream(config):
-    command = f"ffmpeg -f v4l2 -i {config['camera']} -vcodec libx264 -preset veryfast -tune zerolatency -f flv rtmp://{config['serverip']}/{config['app']}/{config['stream']}"
+    command = f"ffmpeg -f v4l2 -i {config['camera']} -vf scale={config['width']}:{config['height']} -vcodec libx264 -preset veryfast -tune zerolatency -f flv rtmp://{config['serverip']}/{config['app']}/{config['stream']}"
     subprocess.run(command, shell=True)
 
 
@@ -10,6 +10,8 @@ if __name__ == '__main__':
         'camera':'/dev/video0',
         'serverip':'localhost',
         'app':'live',
-        'stream':'livestream'
+        'stream':'livestream',
+        'width': 640,
+        'height': 480
     }
     publish_stream(config)
